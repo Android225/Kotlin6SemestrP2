7b1affb08403bd557501051b411a5c238225a636
Andry <a.krishtofor@gmail.com>
2024-04-25 01:58:11 +0300

    Initial commit + add Navigation

---
+++ app/src/main/java/com/example/kotlin6semestr/fragments/MainFragment.kt
@@ -0,0 +1,36 @@
+package com.example.kotlin6semestr.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import com.example.kotlin6semestr.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+
+class  MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val bF1 = view.findViewById<Button>(R.id.button1)
+        val bF2 = view.findViewById<Button>(R.id.button2)
+        val controller = findNavController()
+        bF1.setOnClickListener { controller.navigate(R.id.fragment1) }
+        bF2.setOnClickListener {controller.navigate(R.id.fragment2)}
+    }
+}
